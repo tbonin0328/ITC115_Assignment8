e16cdc199cb02148b36b6a6565dd22914c54a54a
Thomas B <tbonin0328@yahoo.com>
2014-03-12 11:39:46 -0700

    almost there

--- src/MadLibs.java
+++ src/MadLibs.java
@@ -4,51 +4,77 @@ import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+/**
+ * @author thomasbonin
+ * ITC 115 -- Assignment 8, MadLibs
+ */
 public class MadLibs 
 {
-	private static final String ARTICLE_A = " a ";
-	private static final String ARTICLE_B = " an ";
 	private static final String CUE_START = "<";
 	private static final String CUE_END = ">";
 	private static String yourFile = null;
 	
 	private Scanner in = new Scanner(System.in);
 	
+	/**
+	 * @param fileName: the file that's being passed into the method, can be any text file.
+	 * @return: a new file to be read from is returned if it's an actual file.
+	 * This method retrieves the file specified by the method as long as it is a file.
+	 */
 	public Scanner getFileReader(String fileName)
 	{
 		File file = new File(fileName);
-		
 		try 
 		{
 			return new Scanner(file);
 		} 
 		catch (FileNotFoundException e) 
 		{
-			System.out.println(e.getMessage());
-			System.exit(-1);
-			return null;
-		}
+			System.out.println(fileName);
+			System.out.println("Not a valid file name! Please try again.");
+			return (getFileReader("input/" + getFile()));	
+		}				
 	}
 	
+	/**
+	 * @param fileName: the file that's being passed into the method, can be any text file.
+	 * @return: a new file to be written to is returned if it's an actual file.
+	 * This method retrieves a file and sets it up to written to.
+	 */
 	public PrintWriter getFileWriter(String fileName)
 	{
 		FileOutputStream file = null;
 		
 		try
 		{
-		file = new FileOutputStream(fileName);
+			file = new FileOutputStream(fileName);
 		}
 		
 		catch (FileNotFoundException e)
 		{
-		System.out.println(e.getMessage());
-		return null; //System.exit(-1); let's someone decide instead of automatic exit?
+			System.out.println(e.getMessage());
+			return null; //System.exit(-1); let's someone decide instead of automatic exit?
 		}
-		
 		return new PrintWriter(file);
-	//PrintWriter writer = new PrintWriter(file);
 	}
 		
+	/**
+	 * This method (the play method) provides an introduction and then gives the user three options to choose from:
+	 * create, view, or quit. The method uses a while loop set to true that loops as long as the user doesn't choose
+	 * to quit. It uses if statements within this while loop to direct the system to process different methods according
+	 * to user choices of either "create" or "view". Both "create" and "view" use a getFile() method to retrieve the 
+	 * file chosen by the user. Both "create" and "view" use getFileReader to get the chosen file. Only the "create"
+	 * function uses getFileWriter because "view" doesn't need it.
+	 * 
+	 * When the user chooses "create", a while loop processes each line of the chosen file through the
+	 * createMadLib method, as long as there's a next line, and prints each line to the chosen file.
+	 * 
+	 * When the user chooses "view", a while loop is used to pull up each line of the file and print it
+	 * to the console.
+	 * 
+	 * The "create" function ends by closing the writer and the reader. The "view" function ends by 
+	 * closing the reader. 
+	 */
 	public void play()
 	{
 		printString("Welcome to the game of Mad Libs.");
@@ -56,15 +82,15 @@ public class MadLibs
 		
 		while (true)
 		{
-			String command = getString("(C)reate, (V)iew, (Q)uit");
+			String command = getString("(C)reate, (V)iew, or (Q)uit");
 	
 			if (command.toLowerCase().equals("q")) break;
 	
 			if (command.toLowerCase().equals("c"))
-			{
+			{	
 				String file = getFile();
-				PrintWriter writer = getFileWriter("output/" + file);
 				Scanner fileIn = getFileReader("input/" + file);
+				PrintWriter writer = getFileWriter("output/" + file);
 				
 				while(fileIn.hasNextLine())
 				{
@@ -95,82 +121,67 @@ public class MadLibs
 		System.out.println("Goodbye :(");
 	}
 
-		
-		
-		//loop through the file and display its contents
-		
+	/**
+	 * @return: This method returns the file name entered by the user.
+	 * It uses the getString() method to prompt the user to enter the file name.
+	 * It then stores the file as yourFile. The method the creates a new file
+	 * using the file name and checks to make sure it's a file. If it's not a
+	 * file, it prompts the user to enter the file name again until the file name
+	 * is correct, and then it returns the file.
+	 */
 	private String getFile() 
 	{
 		yourFile = getString("Enter file name: ");
-		
 		return yourFile;
 	}
 	
 	/**
-	 * @param line 
 	 * @param string: this string is one line submitted for processing by the method.
-	 * This method: 1) established an index start point of 0;
-	 * 2) gets the index of "<" using the start point as the starting index.
-	 * 3) stores the index of "<" as int x;
-	 * 4) gets the index of ">" using x as the starting index.
-	 * 5) Creates a string called thisCue using x+1 as the starting index and
-	 * y as the ending index.
-	 * 6) Creates a string called article using x-2 as the starting index and x
+	 * This method: 1) initiates several local variables: 
+	 * 		-- int z: a variable that uses the getCueCount method to store the number
+	 * 		of cues in the string.
+	 * 		-- Startpoint, the starting point for each iteration of the indexOf() method
+	 * 		used in a line.
+	 * 		-- int x: the variable used to store the index of the "<".
+	 * 		-- int y: the variable used to to store the index of the ">".
+	 * 		-- String answer: the variable used to store the madlib answer entered by the 
+	 * 		user.
+	 * 2) gets the index of "<" using the indexOf method; stores it as x.
+	 * 3) gets the index of ">" using x as the starting index; stores is as y.
+	 * 4) Creates a string called thisCue using x+1 as the starting index and
+	 * y as the ending index. "thisCue" is the madlib prompt without the greater 
+	 * than/less than signs around it.
+	 * 5) Creates a string called cueReplace using x as the starting index and y+1
 	 * as the ending index.
 	 * 7) 
 	 * @return 
 	 */
 	private String createMadLib(String string) 
 	{
+		int z = getCueCount(string);
+		int Startpoint = 0;
 		int x = 0;
 		int y = 0;
-		int z = getCueCount(string);
 		String answer = "";
-		//String article = "";
-		int Startpoint = 0;
+		String thisCue = "";
+		String cueStart = "";
+		String cueReplace = "";
 		
-			if (z != 0)
-			{
-				for (int i = 0; i < z; i++)
-				{
-					x = string.indexOf(CUE_START, Startpoint); //gets index of next "<";
-					y = string.indexOf(CUE_END, x); //gets index of ">" after "<";
-					String thisCue = string.substring(x+1,y);
-					String cueStart = thisCue.substring(0,1);
-					String cueReplace = string.substring(x, y+1);
-					answer = getAnswer(thisCue, cueStart);
-					string = string.replace(cueReplace, answer);
-					Startpoint = x;
-					//System.out.println(string);
-					//System.out.println(x + ", " + y + ", " + thisCue + ", " + cueReplace);
-				}
-			}
-		return string;
-	}
-	
-	public void viewMadLib(String string)
-	{
-		Scanner fileIn = getFileReader(yourFile);
-		//loop through the file and display its contents
-		while(fileIn.hasNextLine())
+		if (z != 0)
 		{
-			String line = fileIn.nextLine();
-			System.out.println(line);
+			for (int i = 0; i < z; i++)
+			{
+				x = string.indexOf(CUE_START, Startpoint); //gets index of next "<";
+				y = string.indexOf(CUE_END, x); //gets index of ">" after "<";
+				thisCue = string.substring(x+1,y);
+				cueStart = thisCue.substring(0,1);
+				cueReplace = string.substring(x, y+1);
+				answer = getAnswer(thisCue, cueStart);
+				string = string.replace(cueReplace, answer);
+				Startpoint = x;
+			}
 		}
-		
-		fileIn.close();
-		}
-	
-	private String replaceCue(String stringA, String thisCue, String articleC, String cueReplace) 
-	{
-		String cueStart = thisCue.substring(0,1);
-		String answer = "";
-		String answerStart = "";
-		
-		answer = getAnswer(thisCue, cueStart);
-		answerStart = answer.substring(0,1);
-		
-		return (changeString(stringA, answerStart, articleC, cueReplace, answer));
+		return string;
 	}
 	
 	/**
@@ -183,57 +194,22 @@ public class MadLibs
 	 */
 	public String getAnswer(String thisCue, String cueStart)
 	{
-		String answer = "";
+		String prompt = "";
 		
 		while(true)
 		{
 			if (cueStart.toLowerCase().matches("[aeiou]"))
 			{
-				answer = getString("Please type an " + thisCue + ": ");
+				prompt = getString("Please type an " + thisCue + ": ");
 				break;
 			}
 			else
 			{
-				answer = getString("Please type a " + thisCue + ": ");
+				prompt = getString("Please type a " + thisCue + ": ");
 				break;
 			}
 		}
-		return answer;
-	}
-	
-	public String changeString(String stringA, String answerStart, String articleC, String replaceCue, String answer)
-	{
-		String stringB = "";
-		
-		while(true)
-		{
-			if (answerStart.matches("[aeiou]"))
-			{
-				stringB = stringA.replace(ARTICLE_A, ARTICLE_B);
-				stringB = stringB.replace(replaceCue, answer);
-				break;
-			}
-			else
-			{
-				stringB = stringA.replace(replaceCue, answer);
-				break;
-			}
-		}
-		return stringB;
-	}
-	
-	public String getArticle(int x, String string)
-	{
-		String article = "";
-		if(x >= 2)
-		{
-			article = string.substring(x-2, x);
-		}
-		else
-		{
-		article = "the";
-		}
-		return article;
+		return prompt;
 	}
 	
 	public int getCueCount(String string)
@@ -260,9 +236,9 @@ public class MadLibs
 		return in.nextLine();
 	}
 			
-		public static void main(String[] args)
-		{
-			MadLibs program = new MadLibs();
-			program.play();
-		}
+	public static void main(String[] args)
+	{
+		MadLibs program = new MadLibs();
+		program.play();
+	}
 }
